bd7e08048945b0e0d24d2aed0eec74028b007c0d
Shubham Kumar <shubham.kr08092000@gmail.com>
2021-02-19 13:17:19 +0530

    Kanban with firebase added

---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA3OI6z9xjPS31cZ3jqawXkOPKTsCU3VpE",
+    authDomain: "kanbanfire-5e09c.firebaseapp.com",
+    projectId: "kanbanfire-5e09c",
+    storageBucket: "kanbanfire-5e09c.appspot.com",
+    messagingSenderId: "881954089992",
+    appId: "1:881954089992:web:6d90466ab18116159cf5bd",
+    measurementId: "G-DPVWQXP28W"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
